d09bfb92d09f717e8105d0fa62edac71b5821337
Nalbert Schwank Costa Santos <100242852+nalbcosta@users.noreply.github.com>
2025-08-20 13:13:25 +0000

    Refatorar a configuração da chave da API e remover arquivo desnecessário

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let result = document.getElementById('result');
 
 let getMovie = () =>{
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${API_KEY}`;
 
     if(movieName.length <= 0){
         result.innerHTML = `<h3 class= "msg">Por favor informe o nome de um filme</h3>`;
